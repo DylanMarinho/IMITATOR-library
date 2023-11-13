4869c0c2003e003f0ca8b274156343e65dc03816
Dylan Marinho <Dylan.Marinho@loria.fr>
2023-11-13 10:49:22 +0100

    Update CSS

--- scripts/writeHTML.py
+++ scripts/writeHTML.py
@@ -471,8 +471,8 @@ if __name__ == "__main__":
     content += '<title>The IMITATOR benchmark library ({})</title>\n'.format(libraryVersion)
     content += '<meta charset="UTF-8">\n'
     content += '\t<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">\n'
-    # content += '\t<link rel="stylesheet" type="text/css" media="screen" href="http://imitator.fr/imitator.css"/>\n'
-    content += '\t<link rel="stylesheet" type="text/css" media="screen" href="styleLibrary.css"/>\n'
+    content += '\t<link rel="stylesheet" type="text/css" media="screen" href="http://imitator.fr/imitator.css"/>\n'
+    content += '\t<link rel="stylesheet" type="text/css" media="screen" href="library.css"/>\n'
     content += '</head>\n'
     # TODO temp font awesome link, download it for published version
     """Content body with presentation text"""
